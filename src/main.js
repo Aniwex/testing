e34fd39c962181830b51cd9bcb4a13be0d463a69
Aniwex <rekudo55@gmail.com>
2022-09-01 12:03:31 +0300

    server

--- src/main.js
+++ src/main.js
@@ -1,4 +1,20 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const path = require("path");
+const router = jsonServer.router(path.join(__dirname, "login.json"));
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router);
+server.listen(8080, () => {
+  console.log("JSON Server is running");
+});
+
+Vue.config.productionTip = false;
+
+new Vue({
+  render: h => h(App)
+}).$mount("#app");
